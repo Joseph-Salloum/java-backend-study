37f47a2cdb0ebad99d801a34388c40daa0806b32
Joseph <zouzousalloum26@gmail.com>
2026-02-10 20:46:04 -0800

    Week2 is done

--- week2-http-server/src/main/java/com/joseph/backend/Main.java
+++ week2-http-server/src/main/java/com/joseph/backend/Main.java
@@ -13,6 +13,7 @@ public class Main {
         HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
         
         server.createContext("/hello", new HelloHandler());
+        server.createContext("/users", new UsersHandler());
 
         server.setExecutor(null);
         server.start();
@@ -21,12 +22,103 @@ public class Main {
         System.out.flush();
     }
 
+    static class UsersHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            switch (exchange.getRequestMethod()) {
+                case "GET":
+                    getHandler(exchange);
+                    break;
+                default:
+                    defaultHandler(exchange);
+                    break;
+            }
+        }
+
+        private void getHandler(HttpExchange exchange) throws IOException {
+            String[] requestURI = exchange.getRequestURI().getPath().split("/");
+            String response = "";
+            int statusCode = 200;
+
+            switch (requestURI.length) {
+                case 2:
+                    response = getAllUsers();
+                    break;
+                case 3:
+                    String userJson = getUser(requestURI[2]);
+                    if (userJson == null) {
+                        response = """
+                                {
+                                    "message": "No user with the provided ID"
+                                }
+                                """;
+                        statusCode = 404;
+                    } else {
+                        response = userJson;
+                    }
+                    break;
+                default:
+                    response = """
+                            {
+                                "message": "No such endpoint"
+                            }
+                            """;
+                    statusCode = 404;
+                    break;
+            }
+
+            exchange.getResponseHeaders().set("Content-Type", "application/json");
+            exchange.sendResponseHeaders(statusCode, response.getBytes().length);
+
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+        private void defaultHandler(HttpExchange exchange) throws IOException {
+            String response = """
+                    {
+                        "message": "The only supported method is GET"
+                    }
+                    """;
+
+            exchange.getResponseHeaders().set("Content-Type", "application/json");
+            exchange.getResponseHeaders().set("Allow", "GET");
+            exchange.sendResponseHeaders(405, response.getBytes().length);
+
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+
+        private String getAllUsers() {
+            return """
+                    [
+                        {"id": 1, "name": "Joseph"},
+                        {"id": 2, "name": "Diana"}
+                    ]
+                    """;
+        }
+        private String getUser(String id) {
+            if (id.equals("1")) {
+                return """
+                        {"id": 1, "name": "Joseph"}
+                        """;
+            } else if (id.equals("2")) {
+                return """
+                        {"id": 2, "name": "Diana"}
+                        """;
+            } else {
+                return null;
+            }
+        }
+    }
+
     static class HelloHandler implements HttpHandler {
         @Override
         public void handle(HttpExchange exchange) throws IOException {
             switch (exchange.getRequestMethod()) {
                 case "GET":
-                    get(exchange);
+                    getAction(exchange);
                     break;
                 default:
                     defaultAction(exchange);
@@ -35,15 +127,43 @@ public class Main {
             System.out.println(exchange.getRequestMethod() + " " + exchange.getRequestURI());
         }
 
-        private void get(HttpExchange exchange) throws IOException {
-            String response = """
-                {
-                  "message": "Hello from your backend"
-                }
-                """;
+        private void getAction(HttpExchange exchange) throws IOException {
+            String[] requestURI = exchange.getRequestURI().getPath().split("/");
+            String response = "";
+            int statusCode = 200;
+
+            switch (requestURI.length) {
+                case 2:
+                    response = getHelloResponse();
+                    break;
+                case 3:
+                    response = getIdHelloResponse(requestURI[2], false);
+                    break;
+                case 4:
+                    if (!requestURI[3].equals("details")) {
+                        response = """
+                                {
+                                    "message": "No such endpoint"
+                                }
+                                """;
+                        statusCode = 404;
+                        break;
+                    }
+                    response = getIdHelloResponse(requestURI[2], true);
+                    break;
+                default:
+                    response = """
+                            {
+                                "message": "No such endpoint"
+                            }
+                            """;
+                    statusCode = 404;
+                    break;
+            }
+
                     
             exchange.getResponseHeaders().set("Content-Type", "application/json");
-            exchange.sendResponseHeaders(200, response.getBytes().length);
+            exchange.sendResponseHeaders(statusCode, response.getBytes().length);
 
             OutputStream os = exchange.getResponseBody();
             os.write(response.getBytes());
@@ -63,6 +183,29 @@ public class Main {
             OutputStream os = exchange.getResponseBody();
             os.write(response.getBytes());
             os.close();
+        }
+        private String getHelloResponse() {
+            return """
+                    {
+                        "message": "Thanks for using our backend services"
+                    }
+                    """;
+        }
+        private String getIdHelloResponse(String id, boolean withDetails) {
+            if (withDetails) {
+                return String.format("""
+                        {
+                            "message": "Thanks for using our backend services Mr.%s",
+                            "details": "The great backend engineer who wrote this code is Joseph"
+                        }
+                    """, id);
+            }
+
+            return String.format("""
+                    {
+                        "message": "Thanks for using our backend services Mr.%s"
+                    }
+                    """, id);
         }
     }
 }
